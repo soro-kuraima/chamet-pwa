ed6b107c4bb739326e709d87a46e710558a68efa
soro-kuraima <abhi.asno1@gmail.com>
2024-01-19 16:05:43 +0530

    updated start url

---
+++ app/src/main/java/localhost_4173/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package localhost_4173.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
