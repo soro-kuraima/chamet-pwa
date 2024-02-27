192b88eaef767c4367c0cd82c7b165a294a0f6be
soro-kuraima <abhi.asno1@gmail.com>
2024-02-27 21:58:29 +0530

    bug fix

--- src/pages/auth/login/LoginScreenDesktop.tsx
+++ src/pages/auth/login/LoginScreenDesktop.tsx
@@ -13,7 +13,7 @@ export function LoginScreenDesktop({ children }: DeskTopLoginScreenProps) {
       <div className="container relative hidden flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div
-            className="absolute inset-0 h-[95vh] bg-secondary/30 bg-cover bg-no-repeat"
+            className="absolute inset-0 h-[100vh] bg-cover bg-no-repeat"
             style={{
               backgroundImage: `url(${loginBg})`,
             }}
@@ -24,7 +24,7 @@ export function LoginScreenDesktop({ children }: DeskTopLoginScreenProps) {
         </div>
         <div className="lg:p-8">
           <div
-            className="mx-auto flex w-full flex-col items-center justify-center space-y-6 md:w-2/3 md:border-[1rem] md:border-b-0 md:border-r-0 md:border-t-0 md:border-primary md:py-16 md:pl-28 md:shadow-xl"
+            className="mx-auto flex w-full flex-col items-center justify-center space-y-6 md:w-4/5 md:border-[1rem] md:border-b-0 md:border-r-0 md:border-t-0 md:border-primary md:py-16 md:pl-28 md:shadow-xl"
             style={{
               borderRadius: '100% 0% 60% 40% / 47% 100% 0% 53% ',
             }}>
