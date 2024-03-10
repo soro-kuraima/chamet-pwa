2851ae0883d940a70bb347975aa4393f8dd8a118
soro-kuraima <abhi.asno1@gmail.com>
2024-03-10 18:39:07 +0530

    chats card spacing fix

--- src/components/compound/recent-chats-card.tsx
+++ src/components/compound/recent-chats-card.tsx
@@ -25,7 +25,7 @@ const RecentChatsCard = React.forwardRef<HTMLDivElement, RecentChatsCardProps>(
   ) => (
     <div
       ref={ref}
-      className="recent-chats-card my-2 flex items-center justify-between rounded-md bg-[#cce3ff] px-2"
+      className="recent-chats-card my-2 flex items-center gap-6 rounded-md bg-[#cce3ff] px-2"
       role="button"
       tabIndex={0}
       onClick={onTap}
@@ -41,16 +41,19 @@ const RecentChatsCard = React.forwardRef<HTMLDivElement, RecentChatsCardProps>(
         </Avatar>
         <div className="absolute bottom-3 right-2 h-2 w-2 rounded-full bg-green-400 ring-1 ring-white ring-offset-2" />
       </div>
-      <div className="message flex flex-col gap-4">
-        <span className="text-md font-semibold text-black">{userName}</span>
-        <span className="text-sm font-medium text-black/80">{lastMessage}</span>
-      </div>
-      <div className="message flex flex-col items-center gap-4 pb-4">
-        <span className="py-2 text-sm font-medium">{lastMessageTime}</span>
-
-        <span className="text-md flex h-8 w-8 items-center items-center rounded-full bg-primary pl-[11px] pt-1 font-bold text-white">
-          {unreadMessageCount}
-        </span>
+      <div className="flex w-full items-center justify-between">
+        <div className="message flex flex-col gap-4">
+          <span className="text-md font-semibold text-black">{userName}</span>
+          <span className="text-sm font-medium text-black/80">
+            {lastMessage}
+          </span>
+        </div>
+        <div className="message flex flex-col items-center gap-4 pb-4">
+          <span className="py-2 text-sm font-medium">{lastMessageTime}</span>
+          <span className="text-md flex h-8 w-8 items-center items-center rounded-full bg-primary pl-[11px] pt-1 font-bold text-white">
+            {unreadMessageCount}
+          </span>
+        </div>
       </div>
     </div>
   )
